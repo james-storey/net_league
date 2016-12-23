57ba6d42cef7b95db44bd16b4016a593e470d1bf
James Storey <storey.james@gmail.com>
2016-12-22 18:10:26 -0600

    add insert_owner script, ignore journal files

---
+++ insert_owner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sqlite3
+import sys
+
+owners = []
+for i in range(len(sys.argv)):
+    if __file__ != sys.argv[i]:
+        owners.append( (sys.argv[i],) )
+
+print ('inserting owners to netrunner.db')
+conn = sqlite3.connect('netrunner.db')
+conn.executemany("INSERT OR REPLACE INTO owners(owner_name) VALUES (lower(trim(?)))", owners)
+conn.commit()
+conn.close()
