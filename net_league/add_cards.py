eaeff30568223ab409e1265bdf950d2b05f7b705
James Storey <storey.james@gmail.com>
2017-01-23 23:40:58 -0600

    add cards with source pack info into db

--- net_league/add_cards.py
+++ net_league/add_cards.py
@@ -6,7 +6,7 @@ import sys
 
 def run(argv):
     db = 'netrunner.db'
-    input_filename = None
+    input_file = None
     owner = None
 
     for i in range(len(argv)):
@@ -15,18 +15,29 @@ def run(argv):
         elif '-o' == argv[i]:
             owner = argv[i+1]
         if i == len(argv) - 1:
-            input_filename = argv[i]
+            input_file = argv[i]
 
-    input_file = open(input_filename, 'r', encoding='utf-8')
-    card_list = json.load(input_file)
-    input_file.close()
+    pack_name = ''
+    filename = ''
+    if input_file.find('.json') > -1:
+        filename = input_file
+        pack_name = input_filename.split('.')[0]
+    else:
+        filename = input_file + '.json'
+        pack_name = input_file
+    f = open(filename, 'r', encoding='utf-8')
+    card_list = json.load(f)
+    f.close()
 
     conn = sqlite3.connect('netrunner.db')
     for card in card_list:
         for i in range(card['quantity']):
-            conn.execute("INSERT INTO"
-                + " cards(card_name, type, owner, used_in_deck)"
-                + " VALUES(?,?,?,NULL)", [card['title'], card['type_code'], owner])
+            conn.execute('''
+                INSERT INTO cards(
+                    card_name, type, pack, owner, used_in_deck
+                )
+                VALUES(?,?,?,?,NULL)
+            ''', [card['title'], card['type_code'], pack_name, owner])
     conn.commit()
     conn.close()
 
